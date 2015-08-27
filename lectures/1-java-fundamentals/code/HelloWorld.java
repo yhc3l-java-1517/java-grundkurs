e1898a7043ebb30966b3c8dcdefd044401c326b5
Tom Blackmore <tablackmore@gmail.com>
2015-08-27 10:37:15 +0200

    Added lecture 1

---
+++ lectures/1-java-fundamentals/code/HelloWorld.java
@@ -0,0 +1,9 @@
+/* 
+    Hello world Java styleeee
+*/
+    class HelloWorld {
+    // A Java program begins with a call to main()
+        public static void main(String args[]){
+            System.out.println("Hello world from Java");
+        }
+    }
