074c2c69d3ced1fc7c56a71a24686646e5be8e0a
Tom Blackmore <tablackmore@gmail.com>
2015-08-27 13:13:50 +0200

    Added terminal exercise and completed

---
+++ lectures/1-java-fundamentals/exercise-completed/GalToLit.java
@@ -0,0 +1,9 @@
+class GalToLit
+{
+	public static void main(String args[])
+	{
+		double gallons = 10;
+		double liters = 10 * 3.7854;
+		System.out.println("There are " + liters + " liters in " + gallons + " gallons");
+	}
+}
