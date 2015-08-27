72eda37416f3f96fa3eb53df8491145c1a633a52
Tom Blackmore <tablackmore@gmail.com>
2015-08-27 11:31:36 +0200

    Added variable example

---
+++ lectures/1-java-fundamentals/code/Example.java
@@ -0,0 +1,16 @@
+/*
+	A Java program using some variables
+*/
+class Example 
+{
+	public static void main(String args[])
+	{
+		int age;
+		age = 39;
+		int daughtersAge = 10;
+		int ageDifference = age - daughtersAge;
+		System.out.println("My age is " + age);
+		System.out.println("My daughters age is " + daughtersAge);
+		System.out.println("The age difference is " + ageDifference);
+	}
+}
