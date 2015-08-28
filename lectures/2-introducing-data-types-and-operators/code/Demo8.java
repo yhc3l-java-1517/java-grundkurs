54b4b8e62df432f67621d4973d6831d08f838d63
Tom Blackmore <tablackmore@gmail.com>
2015-08-28 14:25:38 +0200

    Updated homework as nettuts tutorial is now paid for

---
+++ lectures/2-introducing-data-types-and-operators/code/Demo8.java
@@ -0,0 +1,17 @@
+class Demo8 {
+	public static void main(String[] args){
+		double x, y;
+		int i;
+
+		x = 10.0;
+		y = 3.0;
+
+		i = (int) (x/y);
+
+		byte b = 80;
+		char c = (char)b;
+
+		System.out.println("char c is " + c);
+
+	}
+}
